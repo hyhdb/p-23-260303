ca6662dcbb01559511ff211cc0f02cc0af08f5ff
hyhdb <kkminwoo@gmail.com>
2026-03-04 15:24:31 +0900

    22

--- src/main/java/com/back/domain/wiseSaying/entity/WiseSaying.java
+++ src/main/java/com/back/domain/wiseSaying/entity/WiseSaying.java
@@ -6,10 +6,24 @@ import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
-@Setter
 public class WiseSaying {
 
+    @Setter
     private int id;
     private String content;
     private String author;
+
+    public WiseSaying(String content, String author) {
+        this.content = content;
+        this.author = author;
+    }
+
+    public void update(String content, String author) {
+        this.content = content;
+        this.author = author;
+    }
+
+    public boolean isNew() {
+        return this.id == 0;
+    }
 }
